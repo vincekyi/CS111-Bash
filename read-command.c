f6d3739b32a547a4bea86c81978ce1334b981a53
Vincent Kyi <kyi@lnxsrv03.seas.ucla.edu>
2013-01-11 01:53:53 -0800

    added command_stream struct and completed read_command_stream

--- read-command.c
+++ read-command.c
@@ -8,10 +8,18 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+//node in the linked list of commands
+struct cmd_node {
+	//contains a pointer to the command and the pointer to the next node
+	command_t cmd;
+	cmd_node* next;
+};
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
 struct command_stream {
-//needs to be an array,vector,list of all commands
+	cmd_node* commands; //list of commands (head)
+	cmd_node* iterator; //iterator to keep track of current iteration
 };
 
 
@@ -19,24 +27,24 @@ command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-char ch_;
-char command_[5000000];
-bzero(command_,5000000);
-ch_ = (char)(*get_next_byte)(get_next_byte_argument);
-int count = 0;
+	char ch_;
+	char command_[5000000];
+	bzero(command_,5000000);
+	ch_ = (char)(*get_next_byte)(get_next_byte_argument);
+	int count = 0;
 
-do {
-	int last = strlen(command_);
-	command_[last] = ch_; 
-	if((ch_ == '\n') || (ch_ == ';')){
-		if(count >= 2){
-			int wsc = 0;
-			while(command_[count - 1 - wsc] == ' ' && ((count - 1 - wsc) > 0 )){ //check cases.. not sure
-				wsc = wsc + 1;
-			}
-			//check if actually command is done
-			switch(command_[count - 1 - wsc])
-			{
+	do {
+		int last = strlen(command_);
+		command_[last] = ch_; 
+		if((ch_ == '\n') || (ch_ == ';')){
+			if(count >= 2){
+				int wsc = 0;
+				while(command_[count - 1 - wsc] == ' ' && ((count - 1 - wsc) > 0 )){ //check cases.. not sure
+					wsc = wsc + 1;
+				}
+				//check if actually command is done
+				switch(command_[count - 1 - wsc])
+				{
 					case '|':
 						//stuff
 						;
@@ -59,18 +67,18 @@ do {
 					default:
 					   	// command is done
 						break;
+				}
 			}
-		}
- 	}	
-	//printf("newline detected, preceeding %c%c\n", command_[count-2], command_[count-1]);
-	count = count + 1;
-	ch_ = (*get_next_byte)(get_next_byte_argument);
-} while (ch_ != EOF);
+ 		}	
+		//printf("newline detected, preceeding %c%c\n", command_[count-2], command_[count-1]);
+		count = count + 1;
+		ch_ = (*get_next_byte)(get_next_byte_argument);
+	} while (ch_ != EOF);
 
  
-printf("%s\n", command_);
+	printf("%s\n", command_);
 // error (1, 0, "command reading not yet implemented");
-  return NULL;
+	return NULL;
 }
 
 command_t
@@ -82,6 +90,15 @@ read_command_stream (command_stream_t s)
 //
 //basically just return 1 command, and remove that
  // error (1, 0, "command reading not yet implemented");
-if (false && s) return NULL;
-  return 0;
+	if(s==NULL)
+		return NULL;
+
+	//iterates through the commands and sets the next iterator
+	if(s->iterator!=NULL){
+		command_t cmd;
+		cmd = s->iterator->cmd;
+		s->iterator = s->iterator->next;
+		//return cmd;
+	}
+	return NULL;
 }
